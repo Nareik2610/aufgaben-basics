4018b1085bce208265ea4a8a4645b0a9a0585b8a
Nareik2610 <Nareik.2610@gmx.de>
2024-12-13 15:12:45 +0100

    Update

--- calculations/numbers/count_divisors.go
+++ calculations/numbers/count_divisors.go
@@ -1,18 +1,20 @@
 package numbers
 
+import "math"
+
 // Erwartet eine Zahl n >= 1 und liefert die Anzahl der Teiler dieser Zahl zurück.
 // TODO
 
 func CountDivisors(n int) int {
 	count := 0
-	
-	for i := 1; i <= n; i++ {
+	for i := 1; i <= int(math.Sqrt(float64(n))); i++ {
 		if n%i == 0 {
-			count++
-			
+			if i*i == n {
+				count++
+			} else {
+				count += 2
 			}
 		}
 	}
 	return count
-}	
-
+}
