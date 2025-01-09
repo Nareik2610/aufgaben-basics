74308011c8f8b95ba8729df2f6d0312a0ea2f80c
Nareik2610 <Nareik.2610@gmx.de>
2025-01-09 18:32:02 +0100

    update

--- cmdline_drawing/rectangles/flexible.go
+++ cmdline_drawing/rectangles/flexible.go
@@ -1,10 +1,54 @@
 package rectangles
 
+import "fmt"
+
 // Erwartet zwei Seitenlängen `height` und `width`.
 // Zeichnet ein Rechteck mit diesen Seitenlängen auf der Konsole.
 // Die Zeichen für Rand und Füllung des Rechtecks werden als Parameter erwartet.
-func DrawRectangle(height, width int, inner, outer string) {
-	// TODO
+func DrawRectangle(width int, height int, randZeichen string, fuellZeichen string, inneresZeichen string) {
+	// Zeichne die obere Grenze
+	for i := 0; i < width; i++ {
+		fmt.Print(randZeichen)
+	}
+	fmt.Println()
+
+	// Zeichne die Seitenränder und die Füllung in der Mitte
+	for i := 0; i < height-2; i++ {
+		fmt.Print(randZeichen)
+		for j := 0; j < width-2; j++ {
+			// Inneres Muster hier: je nach Zeile und Spalte verschiedene Zeichen verwenden
+			if i == (height-2)/2 && j == (width-2)/2 {
+				fmt.Print(inneresZeichen) // Inneres Zeichen in der Mitte
+			} else {
+				fmt.Print(fuellZeichen)
+			}
+		}
+		fmt.Println(randZeichen)
+	}
+
+	// Zeichne die untere Grenze
+	if height > 1 {
+		for i := 0; i < width; i++ {
+			fmt.Print(randZeichen)
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	randZeichen := "#"    // Zeichen für den Rand
+	fuellZeichen := " "   // Zeichen für die Füllung
+	inneresZeichen := "*" // Zeichen in der Mitte
+
+	// Erstes Rechteck
+	width1 := 7
+	height1 := 5
+	DrawRectangle(width1, height1, randZeichen, fuellZeichen, inneresZeichen)
+
+	// Zweites Rechteck (3 x 5)
+	width2 := 5
+	height2 := 3
+	DrawRectangle(width2, height2, randZeichen, fuellZeichen, inneresZeichen)
 }
 
 // REMARKS
