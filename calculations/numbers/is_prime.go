de498ffe9628609d9ae6b785036fd5345abb8043
Nareik2610 <Nareik.2610@gmx.de>
2025-02-22 13:38:58 +0100

    n

--- calculations/numbers/is_prime.go
+++ calculations/numbers/is_prime.go
@@ -1,7 +1,5 @@
 package numbers
 
-import "math"
-
 // Erwartet eine Zahl n und prüft, ob n eine Primzahl ist.
 func IsPrime(n int) bool {
 	// Zahl muss größer als 1 sein, um eine Primzahl zu sein
@@ -10,7 +8,7 @@ func IsPrime(n int) bool {
 	}
 
 	// Nur bis zur Quadratwurzel von n iterieren
-	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+	for i := 2; i < n; i++ {
 		if n%i == 0 {
 			return false // n ist teilbar, also keine Primzahl
 		}
